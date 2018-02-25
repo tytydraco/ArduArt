9241655a79eeed982ef1b556b3c33288e3a8bceb
tytydraco <tylernij@gmail.com>
2018-02-24 18:47:09 -0800

    prototype 1 done

--- main/main.ino
+++ main/main.ino
@@ -1,10 +1,14 @@
 #import <Arduboy2.h>
-#import "keys.h"
 #import "arduart.h"
 
 Arduboy2 arduboy;
-Keys keys(arduboy);
 ArduArt arduart(arduboy);
+
+const int8_t grid_x = arduart.coords[0] * arduart.TILE_SIZE;
+const int8_t grid_y = arduart.coords[1] * arduart.TILE_SIZE;
+
+int8_t points_size = 0;
+int8_t points[128][2] = {};
 
 void setup() {
   arduboy.begin();
@@ -14,28 +18,47 @@ void setup() {
 
 void loop() {
   if (!(arduboy.nextFrame())) return;
+  arduboy.pollButtons();
   arduboy.clear();
-  if (arduboy.everyXFrames(5)) {
-    if (arduboy.pressed(UP_BUTTON)) {
-      arduart.move_me(ArduArt::Dir::UP);
-    }
 
-    if (arduboy.pressed(DOWN_BUTTON)) {
-       arduart.move_me(ArduArt::Dir::DOWN);
-    }
+  if (arduboy.justPressed(UP_BUTTON)) {
+    arduart.move_me(ArduArt::Dir::UP);
+  }
 
-    if (arduboy.pressed(LEFT_BUTTON)) {
-       arduart.move_me(ArduArt::Dir::LEFT);
-    }
+  if (arduboy.justPressed(DOWN_BUTTON)) {
+     arduart.move_me(ArduArt::Dir::DOWN);
+  }
 
-    if (arduboy.pressed(RIGHT_BUTTON)) {
-       arduart.move_me(ArduArt::Dir::RIGHT);
+  if (arduboy.justPressed(LEFT_BUTTON)) {
+     arduart.move_me(ArduArt::Dir::LEFT);
+  }
+
+  if (arduboy.justPressed(RIGHT_BUTTON)) {
+     arduart.move_me(ArduArt::Dir::RIGHT);
+  }
+
+  if (arduboy.justPressed(A_BUTTON)) {
+    points[points_size][0] = arduart.coords[0];
+    points[points_size][1] = arduart.coords[1];
+    points_size++;
+  }
+
+  if (arduboy.justPressed(B_BUTTON)) {
+    int8_t px = arduart.coords[0];
+    int8_t py = arduart.coords[1];
+    for (int i = 0; i < points_size; i++) {
+      if (points[i][0] == px && points[i][1] == py) {
+        points[i][0] = -1;
+        points[i][1] = -1;
+      }
     }
   }
 
-  int8_t grid_x = arduart.coords[0] * arduart.TILE_SIZE;
-  int8_t grid_y = arduart.coords[1] * arduart.TILE_SIZE;
-  arduboy.drawRect(grid_x, grid_y, arduart.TILE_SIZE, arduart.TILE_SIZE, WHITE);
+  for (int i = 0; i < points_size; i++) {
+    arduboy.fillRect(points[i][0] * arduart.TILE_SIZE, points[i][1] * arduart.TILE_SIZE, arduart.TILE_SIZE, arduart.TILE_SIZE, WHITE);
+  }
+  
+  arduboy.drawRect(arduart.coords[0] * arduart.TILE_SIZE, arduart.coords[1] * arduart.TILE_SIZE, arduart.TILE_SIZE, arduart.TILE_SIZE, WHITE);
   
   arduboy.display();
 }
